43aed90f13b2220294d02cd2527e41822d9e6ad6
ravi ranajn <raviranjan2cs@gmail.com>
2022-06-24 00:13:55 +0530

    inital commit

---
+++ ArrayListExample.java
@@ -0,0 +1,21 @@
+package com.ranjan.util;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+	
+	public  ArrayListExample()
+	{
+		
+	}
+	
+	public static void main(String args[])
+	{
+		ArrayList<String> al=new ArrayList<String>();
+		al.add(null);
+		al.add("Ravi");
+		al.add("Sonu");
+		al.forEach(n->System.out.println(n));
+	}
+
+}
